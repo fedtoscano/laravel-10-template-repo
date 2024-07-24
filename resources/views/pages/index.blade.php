158839350ea9faab3abbc3cabf394a8a5a812d5c
fedtoscano <federicotoscano611@gmail.com>
2024-07-24 15:40:35 +0200

    fix

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,13 +2,17 @@
 
 
 @section("document-title", "title-text")
+{{-- CODE GOES HERE --}}
 @endsection
 
 @section("custom-styles")
+{{-- CODE GOES HERE --}}
 @endsection
 
 @section("main-content")
+{{-- CODE GOES HERE --}}
 @endsection
 
 @section("custom-scripts")
+{{-- CODE GOES HERE --}}
 @endsection
