38dcb95565e489830e34e9a0a42223b37ffa1865
fedtoscano <federicotoscano611@gmail.com>
2024-07-24 15:39:56 +0200

    aggiornato template con un layout di riferimento e una index

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends("layouts.layout")
+
+
+@section("document-title", "title-text")
+@endsection
+
+@section("custom-styles")
+@endsection
+
+@section("main-content")
+@endsection
+
+@section("custom-scripts")
+@endsection
